29935fb4baa1956be6e4754ad6cbc83fc7a8ccce
Josius <josius.amaro@gmail.com>
2023-11-09 11:29:10 -0300

    trocando classe dto para record

--- src/main/java/pessoal/estudos/quarkus/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/pessoal/estudos/quarkus/quarkussocial/rest/dto/CreateUserRequest.java
@@ -2,8 +2,8 @@ package pessoal.estudos.quarkus.quarkussocial.rest.dto;
 
 import lombok.Data;
 
-@Data
-public class CreateUserRequest {
-    private String name;
-    private Integer age;
+//@Data
+public record CreateUserRequest (String name, Integer age){
+//    private String name;
+//    private Integer age;
 }
